76d42bcff055b1ccc6e7dafab5eda7635ee8e247
taebin5824 <xoqls5824@gmail.com>
2025-08-16 01:58:05 +0900

    first Commit

---
+++ src/test/java/com/taebin/travelsay/TravelsayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taebin.travelsay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelsayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
