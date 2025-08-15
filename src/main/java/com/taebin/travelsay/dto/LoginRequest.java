7bfe64df704d0679f0fe8f0e26a8d52372d1e6ff
taebin5824 <xoqls5824@gmail.com>
2025-08-16 01:48:51 +0900

    feat: 로그인 요청 DTO

---
+++ src/main/java/com/taebin/travelsay/dto/LoginRequest.java
@@ -0,0 +1,8 @@
+package com.taebin.travelsay.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequest(
+        @NotBlank String loginId,
+        @NotBlank String password
+) {}
