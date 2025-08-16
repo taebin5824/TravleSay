9da8e7c4631f5f76511c3e717b891770ae06ac65
taebin5824 <xoqls5824@gmail.com>
2025-08-17 00:42:24 +0900

    refactor: 회원가입 분기처리, 토큰 버전 관리 메서드

--- src/main/java/com/taebin/travelsay/domain/member/MemberRepository.java
+++ src/main/java/com/taebin/travelsay/domain/member/MemberRepository.java
@@ -1,12 +1,32 @@
 package com.taebin.travelsay.domain.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, String> {
     Optional<Member> findByLoginId(String loginId);
+    Optional<Member> findByMemberId(String memberId);
+
     boolean existsByLoginId(String loginId);
     boolean existsByEmail(String email);
     boolean existsByPhoneNumber(String phoneNumber);
+    boolean existsByEmailAndMemberIdNot(String email, String memberId);
+    boolean existsByPhoneNumberAndMemberIdNot(String phoneNumber, String memberId);
+
+    @Query("select m.tokenVersion from Member m where m.loginId = :loginId")
+    Integer findTokenVersionByLoginId(@Param("loginId") String loginId);
+
+    @Modifying
+    @Query("""
+    update Member m
+       set m.tokenVersion = m.tokenVersion + 1
+     where m.memberId = :memberId
+       and m.tokenVersion = :currentVersion
+""")
+    int bumpIfMatchesByMemberId(@Param("memberId") String memberId,
+                                @Param("currentVersion") Integer currentVersion);
 }
