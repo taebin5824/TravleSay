d5255bdcd737b2e1b305f42713a5a438feae6310
taebin5824 <xoqls5824@gmail.com>
2025-08-22 00:59:31 +0900

    feat: My Trip Plan JpaRepository

---
+++ src/main/java/com/taebin/travelsay/domain/trip/TripPlanRepository.java
@@ -0,0 +1,6 @@
+package com.taebin.travelsay.domain.trip;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TripPlanRepository extends JpaRepository<TripPlan, Long> {
+}
