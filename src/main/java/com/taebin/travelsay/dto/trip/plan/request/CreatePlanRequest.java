7a5ebd009c481e0ecae85114f6ddaee7bb69af2a
taebin5824 <xoqls5824@gmail.com>
2025-08-22 00:52:03 +0900

    feat: 플랜 생성 DTO

---
+++ src/main/java/com/taebin/travelsay/dto/trip/plan/request/CreatePlanRequest.java
@@ -0,0 +1,15 @@
+package com.taebin.travelsay.dto.trip.plan.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record CreatePlanRequest (
+        @NotBlank
+        @Size(max = 200)
+        String title,
+
+        @NotNull
+        Boolean isPublic
+){
+}
