5bf274b8820e2628f764bbe5ca860a40a5595895
taebin5824 <xoqls5824@gmail.com>
2025-08-22 00:56:36 +0900

    feat: 여행일정 상세 수정 DTO

---
+++ src/main/java/com/taebin/travelsay/dto/trip/item/request/UpdateItemRequest.java
@@ -0,0 +1,16 @@
+package com.taebin.travelsay.dto.trip.item.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalTime;
+
+public record UpdateItemRequest(
+        @NotBlank @Size(max = 200) String title,
+        LocalTime startTime,
+        Integer amount,
+        @Size(max = 200) String merchant,
+        @Size(max = 500) String memo
+) {
+
+}
